091c4b7a7c438bcc66ff9badbae991ae3736257c
Vadim Dalecky <streamich@gmail.com>
2023-04-06 19:35:15 +0200

    refactor: 💡 simplify send method interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,8 @@
 import type {TransferListItem} from 'worker_threads';
-import type {WorkerResponse} from '../WorkerResponse';
 
 export * from './messaging';
 
 export type TransferList = ReadonlyArray<TransferListItem>;
 
-export type WpSend<Msg> = (data: Msg | WorkerResponse<Msg>) => void;
+export type WpSend<Msg> = (data: Msg, transferList?: TransferList) => void;
 export type WpRecv<Msg> = (callback: (data: Msg) => void) => void;
